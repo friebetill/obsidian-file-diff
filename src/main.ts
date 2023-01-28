b6d9d694221a779435ffb510f19a48e5c2a3415c
friebetill <friebetill@gmail.com>
2023-01-28 17:55:15 +0800

    Add modal to select file to compare

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,37 @@
-import { Plugin } from "obsidian";
+import { Editor, Plugin, TFile } from "obsidian";
+import { SelectFileModal } from "./select_file_modal";
 
 export default class FileDiffPlugin extends Plugin {
-
 	async onload() {
 		this.addCommand({
 			id: "file-diff",
 			name: "Show the difference between this file and another.",
-			callback: () => {
-				console.log('Hello world!');
-			}
+			editorCallback: async (editor: Editor) => {
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile == null) {
+					return;
+				}
+
+				await this.showSelectOtherFileModal({
+					selectableFiles: this.app.vault.getFiles(),
+				});
+
+				// Install library to calculate the difference between two files
+				// Open new file where the differences are shown
+			},
 		});
 	}
 
+	async showSelectOtherFileModal(args: {
+		selectableFiles: TFile[];
+	}): Promise<TFile | undefined> {
+		return new Promise((resolve, reject) => {
+			return new SelectFileModal(
+				this.app,
+				args.selectableFiles,
+				(error, selectedFile) =>
+					error ? reject(error) : resolve(selectedFile)
+			).open();
+		});
+	}
 }
