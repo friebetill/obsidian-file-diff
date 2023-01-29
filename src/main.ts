81c2e0fb68dd631f2a057d8faa999e48b606d529
friebetill <friebetill@gmail.com>
2023-01-29 23:02:40 +0800

    Show action line

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Editor, Plugin, TFile } from "obsidian";
 
-import { createTwoFilesPatch } from "diff";
+import { structuredPatch } from "diff";
 import { PatchView } from "./patch_view";
 import { SelectFileModal } from "./select_file_modal";
 
@@ -10,14 +10,15 @@ export default class FileDiffPlugin extends Plugin {
 			id: "file-diff",
 			name: "Show the difference between this file and another.",
 			editorCallback: async (editor: Editor) => {
+				// Get current active file
 				const activeFile = this.app.workspace.getActiveFile();
 				if (activeFile == null) {
 					return;
 				}
 
+				// Get file to compare
 				const selectableFiles = this.app.vault.getFiles();
 				selectableFiles.remove(activeFile);
-
 				const fileToCompare = await this.showSelectOtherFileModal({
 					selectableFiles: selectableFiles,
 				});
@@ -25,20 +26,28 @@ export default class FileDiffPlugin extends Plugin {
 					return;
 				}
 
+				// Create difference between the files
 				const activeFileContent = await this.app.vault.read(activeFile);
 				const fileToCompareContent = await this.app.vault.read(
 					fileToCompare
 				);
-
-				const patch = createTwoFilesPatch(
+				const patch = structuredPatch(
 					activeFile.path,
 					fileToCompare.path,
 					activeFileContent,
 					fileToCompareContent
 				);
 
+				// Show difference
 				const workspaceLeaf = this.app.workspace.getLeaf();
-				await workspaceLeaf.open(new PatchView(workspaceLeaf, activeFile, fileToCompare, patch));
+				await workspaceLeaf.open(
+					new PatchView(
+						workspaceLeaf,
+						activeFileContent,
+						fileToCompareContent,
+						patch
+					)
+				);
 			},
 		});
 	}
--- src/patch_view.ts
+++ src/patch_view.ts
@@ -1,13 +1,14 @@
-import { ItemView, TFile, WorkspaceLeaf } from "obsidian";
+import { Hunk, ParsedDiff } from "diff";
+import { ItemView, WorkspaceLeaf } from "obsidian";
 
 export const VIEW_TYPE_PATCH = "patch-view";
 
 export class PatchView extends ItemView {
 	constructor(
 		leaf: WorkspaceLeaf,
-		public file1: TFile,
-		public file2: TFile,
-		public patch: string
+		public file1Content: string,
+		public file2Content: string,
+		public patch: ParsedDiff
 	) {
 		super(leaf);
 	}
@@ -17,16 +18,101 @@ export class PatchView extends ItemView {
 	}
 
 	getDisplayText() {
-		return `Difference between ${this.file1.name} and ${this.file2.name}`;
+		return `Difference between ${this.patch.oldFileName} and ${this.patch.newFileName}`;
 	}
 
 	async onOpen() {
-		const container = this.containerEl.children[1];
-		container.empty();
-		this.contentEl.createDiv({ text: this.patch });
+		const container = this.contentEl.createDiv({ cls: "container" });
+		const lines = this.file1Content.split("\n");
+
+		lines.forEach((line, lineIndex) => {
+			const activeHunk = this.patch.hunks.find((hunk) => {
+				const lineWithMinusIndex = hunk.lines.findIndex((line) =>
+					line.startsWith("-")
+				);
+				return lineIndex === hunk.oldStart - 1 + lineWithMinusIndex;
+			});
+			if (activeHunk != null) {
+				this.buildHunkVisualizer(container, activeHunk);
+			} else {
+				container.createDiv({ text: line, cls: "line" });
+			}
+		});
 	}
 
 	async onClose() {
 		// Nothing to clean up.
+	}
+
+	private buildHunkVisualizer(container: HTMLDivElement, hunk: Hunk) {
+		this.buildActionLine(container);
+
+		hunk.lines.forEach((line) => {
+			if (line.startsWith("+")) {
+				container.createDiv({
+					text: `${line.slice(1, line.length)}`,
+					cls: "line bg-turquoise-light",
+				});
+			} else if (line.startsWith("-")) {
+				container.createDiv({
+					text: `${line.slice(1, line.length)}`,
+					cls: "line bg-blue-light",
+				});
+			}
+		});
+	}
+
+	private buildActionLine(container: HTMLDivElement) {
+		const actionLine = container.createDiv({ cls: "flex-row gap-2 py-2" });
+
+		this.buildActionLineButton(
+			actionLine,
+			"Accept Top",
+			this.handleAcceptTopClick
+		);
+		this.buildActionLineDivider(actionLine);
+		this.buildActionLineButton(
+			actionLine,
+			"Accept Bottom",
+			this.handleAcceptBottomClick
+		);
+		this.buildActionLineDivider(actionLine);
+		this.buildActionLineButton(
+			actionLine,
+			"Accept All",
+			this.handleAcceptAllClick
+		);
+	}
+
+	private buildActionLineButton(
+		hunkActionsLine: HTMLDivElement,
+		text: string,
+		onClick: (event: MouseEvent) => void
+	) {
+		hunkActionsLine
+			.createEl("a", { text, cls: "no-decoration text-xxs text-gray" })
+			.onClickEvent(onClick);
+	}
+
+	private buildActionLineDivider(hunkActionsLine: HTMLDivElement) {
+		hunkActionsLine.createEl("span", {
+			text: "|",
+			cls: "text-xxs text-gray",
+		});
+	}
+
+	private handleAcceptTopClick(event: MouseEvent): void {
+		event.preventDefault();
+		console.log("Accept top");
+	}
+
+	private handleAcceptBottomClick(event: MouseEvent): void {
+		event.preventDefault();
+		console.log("Accept bottom");
+	}
+
+	private handleAcceptAllClick(event: MouseEvent): void {
+		event.preventDefault();
+		console.log("Accept all");
 	}
 }
